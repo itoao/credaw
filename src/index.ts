ae5228e75eea6b5ab8da609613cb081689fb2ad5
aoito <ao.ito@tabian.co>
2024-08-22 21:13:14 +0900

    ♻️ add aws-region.ts

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,12 @@ import { colorize } from 'consola/utils'
 import { runMain as _runMain, defineCommand } from 'citty'
 import { confirm, input, password, select } from '@inquirer/prompts'
 import { readPackageJSON } from 'pkg-types'
+import { awsRegions } from './constants/aws-region'
 
 const { HOME, USERPROFILE } = process.env
 const awsDir = path.join(HOME || USERPROFILE || '', '.aws')
 const credentialsPath = path.join(awsDir, 'credentials')
 const configPath = path.join(awsDir, 'config')
-
-const awsRegions = [
-  'us-east-1',
-  'us-east-2',
-  'us-west-1',
-  'us-west-2',
-  'af-south-1',
-  'ap-east-1',
-  'ap-south-1',
-  'ap-south-2',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'ap-southeast-3',
-  'ap-southeast-4',
-  'ap-northeast-1',
-  'ap-northeast-2',
-  'ap-northeast-3',
-  'ca-central-1',
-  'eu-central-1',
-  'eu-central-2',
-  'eu-west-1',
-  'eu-west-2',
-  'eu-west-3',
-  'eu-south-1',
-  'eu-south-2',
-  'eu-north-1',
-  'me-south-1',
-  'me-central-1',
-  'sa-east-1',
-]
 
 const mainCommand = defineCommand({
   meta: {
